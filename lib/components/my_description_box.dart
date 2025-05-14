695572cfb23114537b8bac5111cd2f7bb323d120
Nahidul Arafat <nahidularaf@gmail.com>
2025-05-14 22:08:31 +0600

    Your message here

--- lib/components/my_description_box.dart
+++ lib/components/my_description_box.dart
@@ -5,34 +5,42 @@ class MyDescriptionBox extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-
     // textStyle
-    
+    var myPrimaryTextStyle = TextStyle(
+      color: Theme.of(context).colorScheme.inversePrimary,
+    );
+    var mySecondaryTextStyle = TextStyle(
+      color: Theme.of(context).colorScheme.primary,
+    );
 
     return Container(
       decoration: BoxDecoration(
         border: Border.all(color: Theme.of(context).colorScheme.secondary),
-      ), // BoxDecoration
+      ),
+      padding: const EdgeInsets.all(25),
+      margin: const EdgeInsets.only(left: 25, right: 25, bottom: 25),
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
           // delivery_fee
           Column(
             children: [
-              Text('\$0.99'),
+              Text('\$0.99', style: myPrimaryTextStyle),
+              Text('Delivery fee', style: mySecondaryTextStyle),
               Text('Delivery fee'),
             ],
-          ), // Column
+          ),
 
           // delivery_time
           Column(
             children: [
-              Text('15-30 min'),
+              Text('15-30 min', style: myPrimaryTextStyle),
+              Text('Delivery time', style: mySecondaryTextStyle),
               Text('Delivery time'),
             ],
-          ), // Column
+          ),
         ],
-      ), // Row
-    ); // Container
+      ),
+    );
   }
 }
